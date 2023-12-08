e1484cbb8a4e4b9fc0c5db05c1fe9687df0b5169
David Aldrin Mondero <aldrinmondero09@gmail.com>
2023-12-08 14:04:15 +0800

    Added function for back button

--- app/src/main/java/com/mondero/david/block1/p1/quiz/ClickActivity.kt
+++ app/src/main/java/com/mondero/david/block1/p1/quiz/ClickActivity.kt
@@ -1,11 +1,23 @@
 package com.mondero.david.block1.p1.quiz
 
+import android.content.Intent
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ClickActivity : AppCompatActivity() {
+
+    private lateinit var back: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_click)
+
+        back = findViewById(R.id.bt_back)
+        back.setOnClickListener{
+            val intent = Intent(this, MainActivity :: class.java)
+            startActivity(intent)
+        }
     }
 }
