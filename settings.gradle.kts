fc73e5f03c1574eed458b788d2d68c0367e1b6f4
David Aldrin Mondero <aldrinmondero09@gmail.com>
2023-12-08 13:56:33 +0800

    Initial Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "DavidMondero-Block1-Quiz"
+include(":app")
+ 
