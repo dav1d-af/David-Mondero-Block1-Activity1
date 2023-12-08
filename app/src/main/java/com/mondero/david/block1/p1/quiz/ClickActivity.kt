4291c40494e651cf3e0bfa3848a24fe603aea61c
David Aldrin Mondero <aldrinmondero09@gmail.com>
2023-12-08 14:01:15 +0800

    Layout the click XML

---
+++ app/src/main/java/com/mondero/david/block1/p1/quiz/ClickActivity.kt
@@ -0,0 +1,11 @@
+package com.mondero.david.block1.p1.quiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ClickActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_click)
+    }
+}
